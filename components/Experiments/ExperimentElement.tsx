edf95f77a7cb0555dcfb4ed0892abfd257dc2dca
Alber <arblugo@gmail.com>
2022-07-03 12:58:58 +0100

    feat: add experiments page & element component

---
+++ components/Experiments/ExperimentElement.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+import { Card, Divider, Link } from "@nextui-org/react";
+import React from "react";
+import { CustomCard } from "../Common/CustomCard";
+import styles from "./ExperimentElement.module.css";
+
+export type ExperimentElementProps = {
+  imgSource: string;
+  imgAlt: string;
+  href: string;
+  cta: string;
+  description?: string;
+  size?: "regular" | "large";
+};
+
+export const ExperimentElement = ({
+  imgSource,
+  imgAlt,
+  href,
+  size = "regular",
+  cta,
+  description,
+}: ExperimentElementProps) => {
+  return (
+    <a href={href} target="_blank" rel="noreferrer">
+      <CustomCard className={styles.element}>
+        <Card.Body>
+          <img
+            src={imgSource}
+            alt={imgAlt}
+            className={size === "large" ? styles.imageLarge : styles.image}
+          />
+          <Divider />
+          <h2>{cta}</h2>
+          {description && <p>{description}</p>}
+        </Card.Body>
+      </CustomCard>
+    </a>
+  );
+};
